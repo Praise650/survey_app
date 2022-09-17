0989332f68705a307f900ccb343feb4f6e27597d
hp <ayokanmi1275@gmail.com>
2022-09-17 21:03:21 +0100

    added buttons to home screen for specific survey

--- lib/ui/screens/homepage/homepage.dart
+++ lib/ui/screens/homepage/homepage.dart
@@ -9,7 +9,25 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BaseScaffold(
       body: BaseScaffoldBody(
-        child: Container(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: const [
+                Text('Health'),
+                Text('Sport'),
+              ],
+            ),
+            Row(
+              children: const [
+                Text('Finance'),
+                Text('Entertainment'),
+              ],
+            ),
+            Row(),
+          ],
+        ),
       ),
     );
   }
